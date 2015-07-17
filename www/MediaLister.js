e1b901c598a752a96bc97407a9f2a2e7c95f00de
Daniel Perez <daniel@claudetech.com>
2015-07-17 18:26:59 +0900

    Avoid modifying passed options.

--- www/MediaLister.js
+++ www/MediaLister.js
@@ -1,32 +1,53 @@
 var exec = require('cordova/exec');
 
+function clone(source) {
+  var cloned = Object.create(source);
+  for (var key in source ) {
+    if (source.hasOwnProperty(key)) {
+      var value = source[key];
+      if (Object.prototype.toString.call(value) === '[object Object]') {
+        cloned[key] = clone(value);
+      } else {
+        cloned[key] = value;
+      }
+    }
+  }
+  return cloned;
+}
+
 exports.readLibrary = function(options, success, error) {
   if (typeof options === 'function') {
     error = success;
     success = options;
     options = {};
   }
+  var originalOptions = options;
+  options = clone(originalOptions);
 
-  if (options.thumbnail) {
+  if (options.thumbnail && !options.thumbnailSize) {
     if (typeof options.thumbnail === 'boolean') {
       options.thumbnailSize = {width: 400, height: 400};
     } else if (typeof options.thumbnail === 'number') {
-      options.thumbnailSize = {width: options.thumbnail, height: options.thumbnail};
+      options.thumbnailSize = {
+        width: options.thumbnail,
+        height: options.thumbnail
+      };
     } else if (typeof options.thumbnail === 'object') {
-      options.thumbnailSize = options.thumbnail;
-      options.thumbnailSize.width = options.thumbnailSize.width || options.thumbnailSize.height || 400;
-      options.thumbnailSize.height = options.thumbnailSize.height || options.thumbnailSize.width || 400;
+      var size = options.thumbnailSize = options.thumbnail;
+      size.width = size.width || size.height || 400;
+      size.height = size.height || size.width || 400;
     }
     options.thumbnail = true;
   }
 
   var originalSuccess = success;
   success = function (entries) {
+    var nextOptions = clone(options);
     var lastEntry = entries[entries.length - 1];
     if (lastEntry) {
-      options.addedBefore = lastEntry.dateAdded;
+      nextOptions.addedBefore = lastEntry.dateAdded;
     }
-    originalSuccess({entries: entries, nextOptions: options});
+    originalSuccess({entries: entries, nextOptions: nextOptions});
   };
 
   exec(success, error, "MediaLister", "readLibrary", [options]);
